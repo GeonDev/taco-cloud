2104da310c02d594ed90e214c47a91cb42b97db9
SonGeon <geons.dev@gmail.com>
2022-11-23 00:52:48 +0900

    스프링 프로 파일 정리

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,7 +1,5 @@
 package tacos.web;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -22,30 +24,58 @@ import org.springframework.validation.Errors;
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
-	
+	private OrderProps props;
+
 	private OrderRepository orderRepo;
-	
-	public OrderController(OrderRepository orderRepo) {
+
+	public OrderController(OrderRepository orderRepo, OrderProps props) {
 		this.orderRepo = orderRepo;
+		this.props = props;
 	}
-	
+
 	@GetMapping("/current")
-	public String orderForm() {
+	public String orderForm(@AuthenticationPrincipal User user,
+							@ModelAttribute Order order) {
+		if (order.getDeliveryName() == null) {
+			order.setDeliveryName(user.getFullname());
+		}
+		if (order.getDeliveryStreet() == null) {
+			order.setDeliveryStreet(user.getStreet());
+		}
+		if (order.getDeliveryCity() == null) {
+			order.setDeliveryCity(user.getCity());
+		}
+		if (order.getDeliveryState() == null) {
+			order.setDeliveryState(user.getState());
+		}
+		if (order.getDeliveryZip() == null) {
+			order.setDeliveryZip(user.getZip());
+		}
 		return "orderForm";
 	}
-	
+
 	@PostMapping
-	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
-								@AuthenticationPrincipal User user) {
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus
+			, @AuthenticationPrincipal User user) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
 
 		order.setUser(user);
-		
+
 		orderRepo.save(order);
 		sessionStatus.setComplete();
-		
+
 		return "redirect:/";
+	}
+
+	@GetMapping
+	public String ordersForUser(
+			@AuthenticationPrincipal User user, Model model) {
+
+		Pageable pageable = PageRequest.of(0, props.getPageSize());
+		model.addAttribute("orders",
+				orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
 	}
 }
